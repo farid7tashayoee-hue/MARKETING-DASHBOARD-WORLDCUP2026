bcbd8312e164c6eef68423413adab6e31ae3eef6
Farid Tashayoee <farid7tashayoee@gmail.com>
2026-08-16 14:56:17 +0330

    fix: show app div after login

--- js/campaigns.js
+++ js/campaigns.js
@@ -600,7 +600,7 @@ async function init() {
     gate.style.opacity = '0';
     setTimeout(() => {
       gate.style.display = 'none';
-      $id('app').style.display = '';
+      $id('app').style.display = 'block';
     }, 420);
   }
   if (localStorage.getItem('l24_auth') === '1') hidGate();
